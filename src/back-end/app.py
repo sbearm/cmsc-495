a635670197cc68dcc02445100d8dc7503df88569
Simon Ebelhar <sebelhar@student.umgc.edu>
2021-03-28 18:14:42 -0700

    'test'

--- src/back-end/app.py
+++ src/back-end/app.py
@@ -1,6 +1,8 @@
 
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, session, redirect, make_response
+import jwt
+import datetime
 from flask_cors import CORS, cross_origin
 from database import Database
 import sqlite3
@@ -9,8 +11,23 @@ from models import Student
 app = Flask(__name__)
 cors = CORS(app)
 
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
+
 db = Database()
 
+@app.route("/")
+def testing():
+    return redirect("/login", code=302)
+
+@app.route("/login")
+def login():
+    auth = request.authorization
+
+    if auth and auth.username == 'simon' and auth.password == 'password':
+        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'token' : token.decode('UTF-8')}) and redirect("/student", code=302)
+
+    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
 
 @app.route("/student")
 def get_all_student():
@@ -21,7 +38,7 @@ def get_all_student():
     return jsonify({
         'data': [result.serialized for result in students]
     })
-# student test
+
 @app.route("/student/<id>")
 def get_student(id):
     resp = db.query_single('select id, name, email from users where Id = ?', id)
@@ -34,6 +51,6 @@ def query_resp(data, status=200):
         return {'data': None, 'error': 'no records found'}, 404
     return {'data': data, 'error': None}, status
 
-
+#comment
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
