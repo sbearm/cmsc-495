03a2ecee03976a527efa768484a34e0d51778e41
sebelhar@student.umgc.edu <sebelhar@student.umgc.edu>
2021-04-08 19:09:55 -0700

    update

--- src/back-end/app.py
+++ src/back-end/app.py
@@ -1,56 +1,159 @@
 
 import json
 from flask import Flask, jsonify, request, session, redirect, make_response
+from flask_sqlalchemy import SQLAlchemy
 import jwt
-import datetime
+from datetime import datetime, timedelta
 from flask_cors import CORS, cross_origin
 from database import Database
 import sqlite3
 from models import Student
+from functools import wraps
+import uuid
+from  werkzeug.security import generate_password_hash, check_password_hash
 
+# creates Flask object
 app = Flask(__name__)
+
 cors = CORS(app)
+# configuration
+# NEVER HARDCODE YOUR CONFIGURATION IN YOUR CODE
+# INSTEAD CREATE A .env FILE AND STORE IN IT
+app.config['SECRET_KEY'] = 'vassa'
+# database name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# creates SQLALCHEMY object
+db = SQLAlchemy(app)
 
-app.config['SECRET_KEY'] = 'thisisthesecretkey'
+# Database ORMs
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	public_id = db.Column(db.String(50), unique = True)
+	name = db.Column(db.String(100))
+	email = db.Column(db.String(70), unique = True)
+	password = db.Column(db.String(80))
 
-db = Database()
+# decorator for verifying the JWT
+def token_required(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		token =  request.headers['Authorization']
+		# jwt is passed in the request header
+		print (request.headers)
+		if 'Authorization' in request.headers:
+			token = request.headers['Authorization']
+		# return 401 if token is not passed
+		if not token:
+			return jsonify({'message' : 'Token is missing'}), 401
+		try:
+			# decoding the payload to fetch the stored details
+			data = jwt.decode(token, app.config['SECRET_KEY'])
+			current_user = User.query\
+				.filter_by(public_id = data['public_id'])\
+				.first()
+		except:
+			return jsonify({
+				'message' : 'Token is invalid'
+			}), 401
+		# returns the current logged in users contex to the routes
+		return f(current_user, *args, **kwargs)
 
-@app.route("/")
-def testing():
-    return redirect("/login", code=302)
+	return decorated
 
-@app.route("/login")
+# User Database Route
+# this route sends back list of users users
+@app.route('/user', methods =['GET'])
+@token_required
+def get_all_users(current_user):
+	# querying the database
+	# for all the entries in it
+	users = User.query.all()
+	# converting the query objects
+	# to list of jsons
+	output = []
+	for user in users:
+		# appending the user data json
+		# to the response list
+		output.append({
+			'public_id': user.public_id,
+			'name' : user.name,
+			'email' : user.email
+		})
+
+	return jsonify({'users': output})
+
+# route for loging user in
+@app.route('/login', methods =['POST'])
 def login():
-    auth = request.authorization
+	# creates dictionary of form data
+	auth = request.form
 
-    if auth and auth.username == 'simon' and auth.password == 'password':
-        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-        return jsonify({'token' : token.decode('UTF-8')}) and redirect("/student", code=302)
+	if not auth or not auth.get('email') or not auth.get('password'):
+		# returns 401 if any email or / and password is missing
+		return make_response(
+			'Could not verify',
+			401,
+			{'WWW-Authenticate' : 'Basic realm ="Login required"'}
+		)
 
-    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+	user = User.query\
+		.filter_by(email = auth.get('email'))\
+		.first()
 
-@app.route("/student")
-def get_all_student():
-    resp = db.query_all('select id, name, email from users')
-    students = []
-    for student in resp:
-        students.append(Student(data=student))
-    return jsonify({
-        'data': [result.serialized for result in students]
-    })
+	if not user:
+		# returns 401 if user does not exist
+		return make_response(
+			'Could not verify',
+			401,
+			{'WWW-Authenticate' : 'Basic realm ="User does not exist"'}
+		)
 
-@app.route("/student/<id>")
-def get_student(id):
-    resp = db.query_single('select id, name, email from users where Id = ?', id)
-    result = Student(data=resp)
-    return jsonify(result.serialized)
+	if check_password_hash(user.password, auth.get('password')):
+		# generates the JWT Token
+		token = jwt.encode({
+			'public_id': user.public_id,
+			'exp' : datetime.utcnow() + timedelta(minutes = 30)
+		}, app.config['SECRET_KEY'])
 
+		return make_response(jsonify({'token' : token}), 201) #.decode('UTF-8')
+	# returns 403 if password is wrong
+	return make_response(
+		'Could not verify',
+		403,
+		{'WWW-Authenticate' : 'Basic realm ="Wrong Password"'}
+	)
 
-def query_resp(data, status=200):
-    if isinstance(data, list) and len(data) == 0:
-        return {'data': None, 'error': 'no records found'}, 404
-    return {'data': data, 'error': None}, status
+# signup route
+@app.route('/signup', methods =['POST'])
+def signup():
+	# creates a dictionary of the form data
+	data = request.form
 
-#comment
+	# gets name, email and password
+	name, email = data.get('name'), data.get('email')
+	password = data.get('password')
+
+	# checking for existing user
+	user = User.query\
+		.filter_by(email = email)\
+		.first()
+	if not user:
+		# database ORM object
+		user = User(
+			public_id = str(uuid.uuid4()),
+			name = name,
+			email = email,
+			password = generate_password_hash(password)
+		)
+		# insert user
+		db.session.add(user)
+		db.session.commit()
+
+		return make_response('Successfully registered.', 201)
+	else:
+		# returns 202 if user already exists
+		return make_response('User already exists. Please Log in.', 202)
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
