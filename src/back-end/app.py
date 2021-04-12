e2b522433730fcdfdca48bc7bedf21c5afcb3a29
sebelhar@student.umgc.edu <sebelhar@student.umgc.edu>
2021-04-11 19:25:00 -0700

    working!

--- src/back-end/app.py
+++ src/back-end/app.py
@@ -35,30 +35,13 @@ class User(db.Model):
 	password = db.Column(db.String(80))
 
 # decorator for verifying the JWT
-def token_required(f):
-	@wraps(f)
-	def decorated(*args, **kwargs):
-		token =  request.headers['Authorization']
-		# jwt is passed in the request header
-		print (request.headers)
-		if 'Authorization' in request.headers:
-			token = request.headers['Authorization']
-		# return 401 if token is not passed
-		if not token:
-			return jsonify({'message' : 'Token is missing'}), 401
-		try:
-			# decoding the payload to fetch the stored details
-			data = jwt.decode(token, app.config['SECRET_KEY'])
-			current_user = User.query\
-				.filter_by(public_id = data['public_id'])\
-				.first()
-		except:
-			return jsonify({
-				'message' : 'Token is invalid'
-			}), 401
-		# returns the current logged in users contex to the routes
-		return f(current_user, *args, **kwargs)
-
+def token_required(f):    
+	@wraps(f)    
+	def decorated(*args, **kwargs):        
+		token =  request.headers.get('Authorization')        
+		data = jwt.decode(token, app.config['SECRET_KEY'])        
+		current_user = User.query.filter_by(public_id = data['public_id']).first()        
+		return f(current_user, *args, **kwargs)    
 	return decorated
 
 # User Database Route
