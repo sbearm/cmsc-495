c8252b18450418c5e9ec4104a582d6a724711308
sebelhar@student.umgc.edu <sebelhar@student.umgc.edu>
2021-04-18 10:45:03 -0700

    updated

--- src/back-end/app.py
+++ src/back-end/app.py
@@ -1,7 +1,5 @@
-
 import json
 from flask import Flask, jsonify, request, session, redirect, make_response
-from flask_sqlalchemy import SQLAlchemy
 import jwt
 from datetime import datetime, timedelta
 from flask_cors import CORS, cross_origin
@@ -10,133 +8,77 @@ import sqlite3
 from models import Student
 from functools import wraps
 import uuid
-from  werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# creates Flask object
 app = Flask(__name__)
-
 cors = CORS(app)
-# configuration
-# NEVER HARDCODE YOUR CONFIGURATION IN YOUR CODE
-# INSTEAD CREATE A .env FILE AND STORE IN IT
+
+db = Database()
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = 'vassa'
-# database name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-# creates SQLALCHEMY object
-db = SQLAlchemy(app)
 
-# Database ORMs
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	public_id = db.Column(db.String(50), unique = True)
-	name = db.Column(db.String(100))
-	email = db.Column(db.String(70), unique = True)
-	password = db.Column(db.String(80))
 
-# decorator for verifying the JWT
-def token_required(f):    
-	@wraps(f)    
-	def decorated(*args, **kwargs):        
-		token =  request.headers.get('Authorization')        
-		data = jwt.decode(token, app.config['SECRET_KEY'])        
-		current_user = User.query.filter_by(public_id = data['public_id']).first()        
-		return f(current_user, *args, **kwargs)    
-	return decorated
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        data = jwt.decode(token, app.config['SECRET_KEY'])
+        current_user = 'select name from Users where userID = ?', data['id'].first()  
+        return f(current_user, *args, **kwargs)
+    return decorated
 
-# User Database Route
-# this route sends back list of users users
-@app.route('/user', methods =['GET'])
+
+@app.route('/authTest', methods=['GET'])
 @token_required
-def get_all_users(current_user):
-	# querying the database
-	# for all the entries in it
-	users = User.query.all()
-	# converting the query objects
-	# to list of jsons
-	output = []
-	for user in users:
-		# appending the user data json
-		# to the response list
-		output.append({
-			'public_id': user.public_id,
-			'name' : user.name,
-			'email' : user.email
-		})
+@cross_origin()
+def authTest(current_user):
+    return jsonify({'Data': 'You are authed'})
 
-	return jsonify({'users': output})
 
-# route for loging user in
-@app.route('/login', methods =['POST'])
+@app.route('/register', methods=['POST'])
+@cross_origin()
+def register():
+    auth = request.json
+
+    if(auth['email'] and auth['password']):
+        user = db.query_single(
+            'select * from users where email = ?', [auth['email']])
+
+        if(user):
+            return {'error': 'Email is already in use'}, 400
+
+        hashed_password = generate_password_hash(auth['password'])
+
+        db.execute('insert into users(name, email, password) values(?, ?, ?)', [
+                   auth['name'], auth['email'], hashed_password])
+
+        return jsonify({'data': 'Successfully registered'})
+
+    else:
+        return {'error': 'Must provide email and password to register'}, 400
+
+
+@app.route('/login', methods=['POST'])
+@cross_origin()
 def login():
-	# creates dictionary of form data
-	auth = request.form
+    auth = request.json
 
-	if not auth or not auth.get('email') or not auth.get('password'):
-		# returns 401 if any email or / and password is missing
-		return make_response(
-			'Could not verify',
-			401,
-			{'WWW-Authenticate' : 'Basic realm ="Login required"'}
-		)
+    if(auth['email'] is None or auth['password']):
 
-	user = User.query\
-		.filter_by(email = auth.get('email'))\
-		.first()
+        user = db.query_single(
+            'select userID, name, email, password from users where email = ?', [auth['email']])
 
-	if not user:
-		# returns 401 if user does not exist
-		return make_response(
-			'Could not verify',
-			401,
-			{'WWW-Authenticate' : 'Basic realm ="User does not exist"'}
-		)
+        if not user:
+            return jsonify({'error': 'No records found for email'}, 401)
 
-	if check_password_hash(user.password, auth.get('password')):
-		# generates the JWT Token
-		token = jwt.encode({
-			'public_id': user.public_id,
-			'exp' : datetime.utcnow() + timedelta(minutes = 30)
-		}, app.config['SECRET_KEY'])
+        if check_password_hash(user[3], auth['password']):
+            token = jwt.encode({'Id': user[0], 'exp': datetime.utcnow(
+            ) + timedelta(minutes=30)}, app.config['SECRET_KEY']).decode("utf-8")
+            return jsonify({'token': token})
+    else:
+        return jsonify({'Error': 'Need to provide credentials'})
 
-		return make_response(jsonify({'token' : token}), 201) #.decode('UTF-8')
-	# returns 403 if password is wrong
-	return make_response(
-		'Could not verify',
-		403,
-		{'WWW-Authenticate' : 'Basic realm ="Wrong Password"'}
-	)
-
-# signup route
-@app.route('/signup', methods =['POST'])
-def signup():
-	# creates a dictionary of the form data
-	data = request.form
-
-	# gets name, email and password
-	name, email = data.get('name'), data.get('email')
-	password = data.get('password')
-
-	# checking for existing user
-	user = User.query\
-		.filter_by(email = email)\
-		.first()
-	if not user:
-		# database ORM object
-		user = User(
-			public_id = str(uuid.uuid4()),
-			name = name,
-			email = email,
-			password = generate_password_hash(password)
-		)
-		# insert user
-		db.session.add(user)
-		db.session.commit()
-
-		return make_response('Successfully registered.', 201)
-	else:
-		# returns 202 if user already exists
-		return make_response('User already exists. Please Log in.', 202)
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
--- src/back-end/database.py
+++ src/back-end/database.py
@@ -4,9 +4,16 @@ import sqlite3
 class Database():
 
     def query_all(self, sql):
-        con = sqlite3.connect('../database/db.db')
+        con = sqlite3.connect('C:\\GitHub\\cmsc-495\\src\\database\\db.db')
         return con.execute(sql).fetchall()
 
     def query_single(self, sql, params):
-        con = sqlite3.connect('../database/db.db')
+        con = sqlite3.connect('C:\\GitHub\\cmsc-495\\src\\database\\db.db')
         return con.execute(sql, params).fetchone()
+
+    def execute(self, sql, params):
+        con = sqlite3.connect('C:\\GitHub\\cmsc-495\\src\\database\\db.db')
+        cur = con.cursor()
+        cur.execute(sql, params)
+        con.commit()
+        return cur.lastrowid
