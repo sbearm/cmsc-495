324aa940c0e51e01915db1944c770abb7d8544fb
sebelhar@student.umgc.edu <sebelhar@student.umgc.edu>
2021-05-02 17:44:34 -0700

    Phase 3 v2 update

--- src/back-end/models.py
+++ src/back-end/models.py
@@ -92,3 +92,40 @@ class ClassDetail:
             'lastname': self.lastname,
             'departmentName': self.departmentName           
         }
+
+class TeacherClass:
+
+    def __init__(self, data):
+        self.courseID = data[0]
+        self.section = data[1]
+        self.hours = data[2]
+        self.courseName = data[3]
+        self.instructorName = data[4]
+        self.students = []
+
+    @property
+    def serialized(self):
+        return {
+            'courseID': self.courseID,
+            'section': self.section,
+            'hours': self.hours,
+            'courseName': self.courseName,
+            'instructorName': self.instructorName,
+            'students': self.students
+        }
+
+class TeacherStudents:
+    def __init__(self, data):
+        self.courseID = data[0]
+        self.enrollmentID = data[1]
+        self.firstName = data[2]
+        self.lastName = data[3]
+
+    @property
+    def serialized(self):
+        return {
+            'courseID': self.courseID,
+            'enrollmentID': self.enrollmentID,
+            'firstName': self.firstName,
+            'lastName': self.lastName,
+        }
